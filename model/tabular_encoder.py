6f2bd0335e72bcf18e5892dbd8f2b31b26af2713
charlescohanlon <charlescohanlon@gmail.com>
2024-04-02 20:36:02 -0700

    touch up tab encoder

--- model/tabular_encoder.py
+++ model/tabular_encoder.py
@@ -1,11 +1,19 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from math import ceil
+
 
 class TabularEncoder(nn.Module):
     def __init__(
-        self, in_dim, out_dim, num_layers=4, layer_dim=None,
-        act_layer=nn.ReLU, dropout_prob=0.0, use_batch_norm=True
+        self,
+        in_dim,
+        out_dim,
+        num_layers=4,
+        layer_dim=None,
+        act_layer=nn.ReLU,
+        dropout_prob=0.0,
+        use_batch_norm=True,
     ):
         super().__init__()
 
@@ -14,20 +22,28 @@ class TabularEncoder(nn.Module):
 
         # If layer_dim is not provided, linearly increase embedding dim over layers
         if layer_dim is None:
-            layer_dim = [(in_dim + i * ((out_dim - in_dim) // num_layers)) for i in range(1, num_layers)]
+            layer_dim = [
+                # integer division causes issues (not sure why)
+                int(in_dim + i * ((out_dim - in_dim) / num_layers))
+                for i in range(num_layers + 1)
+            ]
 
         # Add dropout layer
         self.dropout = nn.Dropout(p=dropout_prob)
 
         # Add layers with activation and batch normalization
         for i in range(num_layers):
-            self.layers.append(nn.Linear(in_features=in_dim if i == 0 else layer_dim[i - 1], out_features=layer_dim[i]))
+            self.layers.append(
+                nn.Linear(
+                    in_features=layer_dim[i],
+                    out_features=layer_dim[i + 1],
+                )
+            )
             if self.use_batch_norm:
-                self.layers.append(nn.BatchNorm1d(layer_dim[i])) # Hopefully using population statistics
-            self.layers.append(act_layer()) # may want to not include on final layer
+                # Hopefully using population statistics
+                self.layers.append(nn.BatchNorm1d(layer_dim[i]))
 
-        # Add output layer
-        self.layers.append(nn.Linear(in_features=layer_dim[-1], out_features=out_dim))
+            self.layers.append(act_layer())  # may want to not include on final layer
 
     def forward(self, x):
         for layer in self.layers:
