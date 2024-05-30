967e8971e3b9279d034ffcfd27ccafa532049b96
Cameron <cameron.maloney@warriorlife.net>
2024-05-29 21:19:32 -0700

    masked tokens indices sampling test

--- model/test_randperm.py
+++ model/test_randperm.py
@@ -1,6 +1,6 @@
 import torch
 
-def sample_indices_from_tensors_vectorized(num_masked_tokens, num_tokens, B):
+def sample_indices(num_masked_tokens, num_tokens, B):
     random_matrix = torch.rand(B, num_tokens)
     sorted_indices = random_matrix.argsort(dim=1)
     sampled_indices = sorted_indices[:, :num_masked_tokens]
@@ -9,5 +9,5 @@ def sample_indices_from_tensors_vectorized(num_masked_tokens, num_tokens, B):
 num_masked_tokens = 9
 num_tokens = 10
 batch_size = 5
-sampled_indices_tensor = sample_indices_from_tensors_vectorized(num_masked_tokens, num_tokens, batch_size)
-print(sampled_indices_tensor)
+test = sample_indices(num_masked_tokens, num_tokens, batch_size)
+print(test)
