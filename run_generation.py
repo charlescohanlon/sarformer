no torch.cuda.amp.autocast(), not minmax scaling depth in eval_image_log() (fixes bug of not supporting depth)

run_generation.py
@@ -1167,7 +1167,9 @@ def generate(
 
         # Draw several samples using the same conditioning
         for i in range(args.num_variations):
-            with torch.cuda.amp.autocast(dtype=dtype, enabled=dtype != torch.float32):
+            with torch.amp.autocast(
+                "cuda", dtype=dtype, enabled=dtype != torch.float32
+            ):
                 out_dict = gen_sampler.generate(
                     sample,
                     schedule,
@@ -1211,8 +1213,8 @@ def generate(
 
             # Super-resolution
             if gen_sampler_sr is not None:
-                with torch.cuda.amp.autocast(
-                    dtype=dtype, enabled=dtype != torch.float32
+                with torch.amp.autocast(
+                    "cuda", dtype=dtype, enabled=dtype != torch.float32
                 ):
                     sample_sr = create_superres_input(
                         out_dict,

run_training_divae.py
@@ -60,7 +60,7 @@ from torchmetrics.image.inception import InceptionScore
 
 from diffusers.schedulers.scheduling_utils import SchedulerMixin
 import diffusers.schedulers as diffusers_schedulers
-
+from fourm.vq.scheduling import DDPMScheduler, DDIMScheduler  # Don't remove, need these
 import fourm.utils as utils
 from fourm.data import build_wds_divae_dataloader
 from fourm.utils import destandardize
@@ -2428,16 +2428,8 @@ def eval_image_log(
                 gt = destandardize(clean_images[:, :3], mean=NAIP_MEAN, std=NAIP_STD)
                 reconst = destandardize(output[:, :3], mean=NAIP_MEAN, std=NAIP_STD)
             elif domain in ["depth"]:
-                # 1-channel per-sample standardized domains
                 gt = clean_images[:, :1]
-                batch_min = rearrange(gt, "b c h w -> b (c h w)").min(1)[0][
-                    :, None, None, None
-                ]
-                batch_max = rearrange(gt, "b c h w -> b (c h w)").max(1)[0][
-                    :, None, None, None
-                ]
-                gt = (gt - batch_min) / (batch_max - batch_min)
-                reconst = (output[:, :1] - batch_min) / (batch_max - batch_min)
+                reconst = output[:, :1]
             elif domain in [
                 "edge_occlusion",
                 "edge_texture",
@@ -2474,6 +2466,27 @@ def eval_image_log(
                 reconst_bytes = (
                     255 * reconst.float().permute(0, 2, 3, 1).clamp(0, 1).cpu().numpy()
                 ).astype(np.uint8)
+            elif domain in ["depth"]:
+                gt_bytes = (
+                    255
+                    * gt.float()
+                    .repeat(1, 3, 1, 1)  # repeat to 3 channels for visualization
+                    .permute(0, 2, 3, 1)
+                    .clamp(0, 1)
+                    .cpu()
+                    .numpy()
+                ).astype(np.uint8)
+                reconst_bytes = (
+                    255
+                    * reconst.float()
+                    .repeat(1, 3, 1, 1)
+                    .permute(0, 2, 3, 1)
+                    .clamp(0, 1)
+                    .cpu()
+                    .numpy()
+                ).astype(np.uint8)
+            else:
+                raise ValueError(f"Unsupported domain for image logging {domain}")
 
             gt_imgs.extend([Image.fromarray(b) for b in gt_bytes])
             reconst_imgs.extend([Image.fromarray(b) for b in reconst_bytes])

run_training_vqcontrolnet.py
@@ -1500,7 +1500,7 @@ def train_one_epoch(
             clean_input, noise, timesteps
         )
 
-        with torch.cuda.amp.autocast(dtype=dtype, enabled=dtype != torch.float32):
+        with torch.amp.autocast("cuda", dtype=dtype, enabled=dtype != torch.float32):
             model_output, code_loss = model(
                 clean_images, noisy_input, timesteps.to(device), prompt=caption
             )
@@ -1804,7 +1804,7 @@ def evaluate(
             clean_input, noise, timesteps
         )
 
-        with torch.cuda.amp.autocast(dtype=dtype, enabled=dtype != torch.float32):
+        with torch.amp.autocast("cuda", dtype=dtype, enabled=dtype != torch.float32):
             model_output, code_loss = model(
                 clean_images, noisy_input, timesteps.to(device), prompt=caption
             )
@@ -1919,8 +1919,8 @@ def eval_metrics(
         )
 
         # Autoencode the images
-        with torch.no_grad(), torch.cuda.amp.autocast(
-            dtype=dtype, enabled=dtype != torch.float32
+        with torch.no_grad(), torch.amp.autocast(
+            "cuda", dtype=dtype, enabled=dtype != torch.float32
         ):
             output = unwrap_model(model).autoencode(
                 clean_images,
@@ -2052,8 +2052,8 @@ def eval_image_log(
             )
 
             # Autoencode the images
-            with torch.no_grad(), torch.cuda.amp.autocast(
-                dtype=dtype, enabled=dtype != torch.float32
+            with torch.no_grad(), torch.amp.autocast(
+                "cuda", dtype=dtype, enabled=dtype != torch.float32
             ):
                 output = unwrap_model(model).autoencode(
                     clean_images,

run_training_vqvae.py
@@ -1770,15 +1770,15 @@ def train_one_epoch(
         if "sam_mask" in domain and images.shape[0] == 0:
             continue
 
-        with torch.cuda.amp.autocast(dtype=dtype, enabled=dtype != torch.float32):
+        with torch.amp.autocast("cuda", dtype=dtype, enabled=dtype != torch.float32):
             model_output, code_loss = model(images)
 
             # Reconstruction loss
             reconst_loss = compute_reconst_loss(model_output, images, loss_fn)
 
         # Perceptual loss
-        with torch.cuda.amp.autocast(
-            dtype=dtype_percept, enabled=dtype_percept != torch.float32
+        with torch.amp.autocast(
+            "cuda", dtype=dtype_percept, enabled=dtype_percept != torch.float32
         ):
             if percept_loss_fn is not None and percept_loss_weight > 0:
                 transform = unwrap_model(percept_loss_fn).percept_transform
@@ -2123,15 +2123,15 @@ def evaluate(
         if "sam_mask" in domain and images.shape[0] == 0:
             continue
 
-        with torch.cuda.amp.autocast(dtype=dtype, enabled=dtype != torch.float32):
+        with torch.amp.autocast("cuda", dtype=dtype, enabled=dtype != torch.float32):
             model_output, code_loss = model(images)
 
             # Reconstruction loss
             reconst_loss = compute_reconst_loss(model_output, images, loss_fn)
 
         # Perceptual loss
-        with torch.cuda.amp.autocast(
-            dtype=dtype_percept, enabled=dtype_percept != torch.float32
+        with torch.amp.autocast(
+            "cuda", dtype=dtype_percept, enabled=dtype_percept != torch.float32
         ):
             if percept_loss_fn is not None and percept_loss_weight > 0:
                 transform = unwrap_model(percept_loss_fn).percept_transform
@@ -2286,8 +2286,8 @@ def eval_metrics(
             continue
 
         # Autoencode the images
-        with torch.no_grad(), torch.cuda.amp.autocast(
-            dtype=dtype, enabled=dtype != torch.float32
+        with torch.no_grad(), torch.amp.autocast(
+            "cuda", dtype=dtype, enabled=dtype != torch.float32
         ):
             quant, _, tokens = unwrap_model(model).encode(images)
             output = unwrap_model(model).decode_quant(quant).float()
@@ -2510,8 +2510,8 @@ def eval_image_log(
                 continue
 
             # Autoencode the images
-            with torch.no_grad(), torch.cuda.amp.autocast(
-                dtype=dtype, enabled=dtype != torch.float32
+            with torch.no_grad(), torch.amp.autocast(
+                "cuda", dtype=dtype, enabled=dtype != torch.float32
             ):
                 output = unwrap_model(model).autoencode(images)
 